5b85e5ce1f55fd3e9974665af1a961073bcc4434
Ashish Biswas <biswasashish655@gmail.com>
2025-06-13 08:28:03 +0530

    Added the functionality for user to update there data

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,6 +2,16 @@ const User = require('./../Models/UserModel');
 const catchAsync = require('./../utility/catchAsync');
 const AppError = require('./../utility/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
 exports.getAllUser = catchAsync(async (req, res, next) => {
   const user = await User.find();
 
@@ -76,5 +86,30 @@ exports.deleteUser = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'Success',
     message: 'User deleted'
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.confirmpassword) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updateduser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      user: updateduser
+    }
   });
 });
